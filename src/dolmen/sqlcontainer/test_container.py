58bbb5aa1fa41bf17c631da4da83273435f721fb
Souheil CHELFOUH <trollfot@gmail.com>
2013-08-19 18:39:40 +0200

    Added missing file

---
+++ src/dolmen/sqlcontainer/test_container.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def setup_module(module):
+    """ grok the publish module
+    """
+    pass
+
+
+def teardown_module(module):
+    """ undo groking
+    """
+    pass
+
+
+def test_container():
+    pass
